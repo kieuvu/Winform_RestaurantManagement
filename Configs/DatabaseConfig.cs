fd6ad9cee793f211814fe507b43bcd8ea3665f33
kieuvu <kieuvuvuvu@gmail.com>
2023-12-05 23:21:19 +0700

    Add project files.

---
+++ Configs/DatabaseConfig.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RestaurantManagement.Configs
+{
+    internal class DatabaseConfig
+    {
+        private readonly static string _dataSource = "DESKTOP-8CLBSER\\VUKM";
+        private readonly static string _initialCatalog = "QLTS";
+        private readonly static bool _integratedSecurity = true;
+
+        public static string GetConnectionString()
+        {
+            return $"Data Source={_dataSource};"
+                 + $"Initial Catalog={_initialCatalog};"
+                 + $"Integrated Security={_integratedSecurity};"; ;
+        }
+    }
+}
---
+++ Utils/DatabaseUtils.cs
@@ -0,0 +1,32 @@
+﻿using RestaurantManagement.Configs;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace RestaurantManagement.Utils
+{
+    internal class DatabaseUtils
+    {
+        private static readonly string _connectString = DatabaseConfig.GetConnectionString();
+
+        public static SqlDataReader ExecuteSqlQuery(string sqlQuery, SqlParameter[]? parameters = null)
+        {
+            SqlConnection connection = new(_connectString);
+            connection.Open();
+
+            using (SqlCommand sqlCommand = new(sqlQuery, connection))
+            {
+                if (parameters != null)
+                {
+                    sqlCommand.Parameters.AddRange(parameters);
+                }
+
+                return sqlCommand.ExecuteReader(CommandBehavior.CloseConnection);
+            }
+        }
+    }
+}
